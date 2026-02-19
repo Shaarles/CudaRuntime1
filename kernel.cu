757b20ccab822322c5b82a7204016fdd45556091
Charles Guo <charlesguo91@gmail.com>
2026-02-19 22:41:07 +0100

    finished Explicit and shared memory introduction

--- kernel.cu
+++ kernel.cu
@@ -2,58 +2,156 @@
 #include "kernel.h"
 
 
-__global__ void add(int* a, int*b, int* c, int vector_length ){
+__global__ void add(float* a, float*b, float* c, int vector_length ){
 	//Make computations
-	int work_index = threadIdx.x * threadIdx.y;
+	int work_index = blockDim.x*blockIdx.x+ threadIdx.x;
 	// It is possible to verify with a "work index" that you are not getting out of bound if you put too many threads to work
 
 	if (work_index < vector_length) {
-		a[threadIdx.x * threadIdx.y] = blockIdx.x;
-		b[threadIdx.x * threadIdx.y] = blockIdx.y;
-		c[threadIdx.x * threadIdx.y] = blockIdx.x + blockIdx.y;
+		c[work_index] = a[work_index] + b[work_index];
 	}
 }
-/*
+
+void add_cpu_vers(float* a, float* b, float* c, int vector_length) {
+	//Make computations
+	for (int i=0; i < vector_length; i++) {
+		c[i] = a[i] + b[i];
+	}
+}
+
+void initArray(float* arr, int length) {
+	for (int i = 0; i < length; i++) {
+		arr[i] = 1.0f;
+	}
+}
+
+bool compareResults(float* a, float* b, int length) {
+	for (int i = 0; i < length; i++) {
+		if (a[i] != b[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
 void unifiedMemExample(int vectorLength) {
+	//defining add parameters
+
+	int threads = 256;
+	int blocks = (vectorLength + threads - 1) / threads; //Ceil of vectorLength/threads
 
 	//Pointers to memory vectors
 	float* A = nullptr;
 	float* B = nullptr;
 	float* C = nullptr;
+	float* comparisonResult = (float*)malloc(vectorLength * sizeof(float));
 
 	//Using unified memory to allocate buffers
 	cudaMallocManaged(&A, vectorLength * sizeof(float));
 	cudaMallocManaged(&B, vectorLength * sizeof(float));
 	cudaMallocManaged(&C, vectorLength * sizeof(float));
 
-	//Initialize vectors A and B on the host
 	initArray(A, vectorLength);
 	initArray(B, vectorLength);
+	//Measuring time taken to do the operation on the GPU
+	auto start = chrono::high_resolution_clock::now();
+	//Launch kernel on the GPU
+	add << <blocks, threads >> > (A, B, C, vectorLength);
+	auto end = chrono::high_resolution_clock::now();
+	cout << "GPU version took " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " milliseconds" << endl;
+
+
+	//important part incoming!!!!
+	cudaDeviceSynchronize();	//That's so important!!!!
+
+	//Measuring time taken to do the operation on the CPU
+	start = chrono::high_resolution_clock::now();
+	add_cpu_vers(A, B, comparisonResult, vectorLength);
+	end = chrono::high_resolution_clock::now();
+	cout << "CPU version took " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " milliseconds" << endl;
+	
+	cout << compareResults(C, comparisonResult, vectorLength) << endl;
+
+
 	cudaFree(A);
 	cudaFree(B);
 	cudaFree(C);
+	free(comparisonResult);
 }
-*/
+
+void explicitMemExample(int vectorLength) {
+	//defining add parameters
+	int threads = 256;
+	int blocks = (vectorLength + threads - 1) / threads; //Ceil of vectorLength/threads
+
+	//Pointers for host memoy
+	float* A = nullptr;
+	float* B = nullptr;
+	float* C = nullptr;
+	float* comparison = nullptr;
+
+	//Pointers for device memoy
+	float* devA = nullptr;
+	float* devB = nullptr;
+	float* devC = nullptr;
+
+	//Allocate Host Memory using cudaMallocHost API
+	//(best option when buffers will be use for copies between CPU and GPU mem)
+	//(for best performance, use pinned memory for buffers that will be used for copies between CPU and GPU memory)
+	cudaMallocHost(&A, vectorLength * sizeof(float));
+	cudaMallocHost(&B, vectorLength * sizeof(float));
+	cudaMallocHost(&C, vectorLength * sizeof(float));
+	cudaMallocHost(&comparison, vectorLength * sizeof(float));
+
+	//Allocating device memory
+	initArray(A, vectorLength);
+	initArray(B, vectorLength);
+
+	//start allocating and copying memory on the gpu
+	//Allocating memory to GPU
+	cudaMalloc(&devA, vectorLength * sizeof(float));
+	cudaMalloc(&devB, vectorLength * sizeof(float));
+	cudaMalloc(&devC, vectorLength * sizeof(float));
+
+	//Copy data to the GPU
+	cudaMemcpy(devA, A, vectorLength * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(devB, B, vectorLength * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(devC, C, vectorLength * sizeof(float), cudaMemcpyHostToDevice);
+
+	//Measuring time taken to do the operation on the GPU
+	auto start = chrono::high_resolution_clock::now();
+	//Launch kernel on the GPU
+	add << <blocks, threads >> > (devA, devB, devC, vectorLength);
+	auto end = chrono::high_resolution_clock::now();
+	cout << "GPU version took " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " milliseconds" << endl;
+
+
+	//important part incoming!!!!
+	cudaDeviceSynchronize();	//That's so important!!!!
+
+	//Measuring time taken to do the operation on the CPU
+	start = chrono::high_resolution_clock::now();
+	add_cpu_vers(A, B, C, vectorLength);
+	end = chrono::high_resolution_clock::now();
+	cout << "CPU version took " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " milliseconds" << endl;
+
+	cudaMemcpy(comparison, devC, vectorLength * sizeof(float), cudaMemcpyDefault);
+
+	cout << compareResults(C, comparison, vectorLength) << endl;
+	//Freeing all this memory used
+	cudaFree(devA);
+	cudaFree(devB);
+	cudaFree(devC);
+	cudaFreeHost(A);
+	cudaFreeHost(B);
+	cudaFreeHost(C);
+}
+
 int main() {
-	dim3 grid(4, 4);
-	int gridsize = grid.x * grid.y;
-	dim3 block(16, 16);
-	int blockSize = block.x * block.y;
-
-	int* a = (int*)malloc(blockSize * sizeof(int));
-	int* b = (int*)malloc(blockSize * sizeof(int));
-	int* c = (int*)malloc(blockSize * sizeof(int));
-	add <<<grid, block >>> (a, b, c, blockSize);
-
-	for (int i = 0; i < blockSize; i++) {
-		cout << "a[" << i << "] = " << a[i] << endl;
-		cout << "b[" << i << "] = " << b[i] << endl;
-		cout << "c[" << i << "] = " << c[i] << endl;
-	}
-	
-	free(a);
-	free(b);
-	free(c);
+	char a;
+	int n = 20000000;
+	explicitMemExample(50*n);
+	cin >> a;
 
 	return 0;
 }
--- kernel.h
+++ kernel.h
@@ -3,6 +3,6 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <string>
-
+#include <chrono>
 #include <stdio.h>
 using namespace std;
