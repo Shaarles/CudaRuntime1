f619fec9d643035adb13b94a0ac22d4c032668a6
Charles GUO <Charles GUO@LAPTOP-C4IUTUBH>
2026-02-19 08:53:49 +0100

    Ajoutez des fichiers projet.

---
+++ kernel.cu
@@ -0,0 +1,59 @@
+﻿#include "Vector.cpp"
+#include "kernel.h"
+
+
+__global__ void add(int* a, int*b, int* c, int vector_length ){
+	//Make computations
+	int work_index = threadIdx.x * threadIdx.y;
+	// It is possible to verify with a "work index" that you are not getting out of bound if you put too many threads to work
+
+	if (work_index < vector_length) {
+		a[threadIdx.x * threadIdx.y] = blockIdx.x;
+		b[threadIdx.x * threadIdx.y] = blockIdx.y;
+		c[threadIdx.x * threadIdx.y] = blockIdx.x + blockIdx.y;
+	}
+}
+/*
+void unifiedMemExample(int vectorLength) {
+
+	//Pointers to memory vectors
+	float* A = nullptr;
+	float* B = nullptr;
+	float* C = nullptr;
+
+	//Using unified memory to allocate buffers
+	cudaMallocManaged(&A, vectorLength * sizeof(float));
+	cudaMallocManaged(&B, vectorLength * sizeof(float));
+	cudaMallocManaged(&C, vectorLength * sizeof(float));
+
+	//Initialize vectors A and B on the host
+	initArray(A, vectorLength);
+	initArray(B, vectorLength);
+	cudaFree(A);
+	cudaFree(B);
+	cudaFree(C);
+}
+*/
+int main() {
+	dim3 grid(4, 4);
+	int gridsize = grid.x * grid.y;
+	dim3 block(16, 16);
+	int blockSize = block.x * block.y;
+
+	int* a = (int*)malloc(blockSize * sizeof(int));
+	int* b = (int*)malloc(blockSize * sizeof(int));
+	int* c = (int*)malloc(blockSize * sizeof(int));
+	add <<<grid, block >>> (a, b, c, blockSize);
+
+	for (int i = 0; i < blockSize; i++) {
+		cout << "a[" << i << "] = " << a[i] << endl;
+		cout << "b[" << i << "] = " << b[i] << endl;
+		cout << "c[" << i << "] = " << c[i] << endl;
+	}
+	
+	free(a);
+	free(b);
+	free(c);
+
+	return 0;
+}
---
+++ kernel.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <string>
+
+#include <stdio.h>
+using namespace std;
